7cfb0ebf9752c87203fa2d0f541643603a8829fc
digantabandopadhyay@gmail.com <dxb792@student.bham.ac.uk>
2024-03-21 15:32:34 +0000

    Parameter order bug fix

--- SC_search/Search.py
+++ SC_search/Search.py
@@ -252,7 +252,7 @@ class Search:
                                                             self.psd_array,
                                                             self.df,
                                                             self.waveform_func,
-                                                            self.waveform_args) for segment_number in self.segment_ladder]
+                                                            waveform_args=self.waveform_args) for segment_number in self.segment_ladder]
         
         PySO_search = PySO.HierarchicalSwarmHandler(self.Semi_Coherent_classes,
                                 self.PySO_num_swarms,# Number of initial swarms
