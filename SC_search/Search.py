Small fix to PSD normalisation

SC_search/Search.py
@@ -208,10 +208,10 @@ class Search:
         noise = np.zeros((3,self.nT,self.nF),dtype=complex)
     
         for t_index,t in enumerate(self.t_seg):
-
-            noise_A = noise_realization(self.psd_arr[0,t_index,:],self.T_obs)
-            noise_E = noise_realization(self.psd_arr[1,t_index,:],self.T_obs)
-            noise_T = noise_realization(self.psd_arr[2,t_index,:],self.T_obs)
+            # Important thing here is that it is dT not T_obs as that is the size of each segment   
+            noise_A = noise_realization(self.psd_arr[0,t_index,:],self.dT)
+            noise_E = noise_realization(self.psd_arr[1,t_index,:],self.dT)
+            noise_T = noise_realization(self.psd_arr[2,t_index,:],self.dT)
 
             noise[:,t_index,:] = np.array([noise_A,noise_E,noise_T])
 